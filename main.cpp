60df56c6fa773f88ea28b8601229039f95e79f8a
Victor Monje <monjev086@gmail.com>
2023-05-18 22:00:14 -0600

    adding part's 3 & 6

--- main.cpp
+++ main.cpp
@@ -24,9 +24,13 @@ void inputLast(ptr_nodoList *ptr, char value);
 // part 3 - 4
 void inputPosition(ptr_nodoList *ptr, char value, int position);
 void deleteElement(ptr_nodoList *ptr, char value);
+// part 5 - 6
+void printList(ptr_nodoList ptr);
 
 int main()
 {
+
+    ptr_nodoList list = NULL;
     int op;
     do
     {
@@ -42,6 +46,18 @@ int main()
             break;
         case 3:
             /* code */
+            int po;
+            char value;
+            cout << "\n\tLista antes de ingresar ->\n\t";
+            printList(list);
+            cout << "\n\tIngrese la posicion -> ";
+            cin >> po;
+
+            cout << "\tIngrese el valor -> ";
+            cin >> value;
+            inputPosition(&list, value, po);
+            cout << "\n\tLista despues de ingresar ->\n\t";
+            printList(list);
             break;
         case 4:
             /* code */
@@ -51,15 +67,19 @@ int main()
             break;
         case 6:
             /* code */
+            cout << "\n\tImprimiendo Lista ->\n\t";
+            printList(list);
             break;
         case 7:
-            /* code */
+            cout << "\t\nFeliz Dia!\n\n";
             break;
 
         default:
             cout << "error - opcion no valida";
             break;
         }
+        system("pause");
+        system("cls");
 
     } while (op != 7);
     return 0;
@@ -76,4 +96,61 @@ void instructions(void)
     cout << "\t6- Mostrar lista" << endl;
     cout << "\t7- Salir" << endl;
     cout << "\tIngrese su opcion -> ";
+}
+
+void inputPosition(ptr_nodoList *ptr, char value, int position)
+{
+    ptr_nodoList newNode = (ptr_nodoList)malloc(sizeof(NodoLista));
+    if (newNode == NULL)
+    {
+        printf("Error: No se pudo asignar memoria para el nuevo nodo.\n");
+        return;
+    }
+
+    newNode->d = value;
+    newNode->ptr_s = NULL;
+
+    if (*ptr == NULL)
+    {
+        *ptr = newNode;
+        return;
+    }
+
+    if (position == 0)
+    {
+        newNode->ptr_s = *ptr;
+        *ptr = newNode;
+        return;
+    }
+
+    ptr_nodoList current = *ptr;
+    int count = 1;
+
+    while (current->ptr_s != NULL && count < position)
+    {
+        current = current->ptr_s;
+        count++;
+    }
+
+    newNode->ptr_s = current->ptr_s;
+    current->ptr_s = newNode;
+}
+
+void printList(ptr_nodoList ptr)
+{
+    // por si no hay elementos en la lista almacenados
+    if (ptr == NULL)
+    {
+        printf("No hay elementos en la lista.\n");
+        return;
+    }
+
+    // Recoriendo la lista
+    while (ptr != NULL)
+    {
+        printf("%d -> ", ptr->d);
+        ptr = ptr->ptr_s;
+    }
+
+    cout << endl;
 }
